fddf366ec95de59aede6ee7f1cde6b36d7b3d0af
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2019-09-15 01:14:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/around/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.around;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Hotels> hotels = Hotels.getHotels(); //getHotels is the database query
+    ListView lvHotels;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvHotels = findViewById(R.id.lvHotels);
+        HotelAdapter hotelAdapter = new HotelAdapter();
+        lvHotels.setAdapter(hotelAdapter);
+    }
+
+    class HotelAdapter extends BaseAdapter
+    {
+        @Override
+        public int getCount()
+        {
+            return hotels.size();
+        }
+
+        public Hotels getItem(int position)
+        {
+            return hotels.get(position);
+        }
+
+        public long getItemId(int position)
+        {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            View itemView = getLayoutInflater().inflate(R.layout.list_item_hotel, parent, false);
+
+            TextView tvName = itemView.findViewById(R.id.tvName);
+            //TextView tv test = itemView.findViewById(R.id.tvTest);
+            tvName.setText(getItem(position).getName());
+            tvTest.setText(getItem(position.getTest));
+        }
+
+
+
+    }
+
+}
